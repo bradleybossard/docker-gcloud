9d05f3dc3cd1ddcf1494f32069f7c683c9cf976f
Bradley Bossard <bradleybossard@gmail.com>
2016-03-14 23:23:02 +0000

    Updating welcome shell script instructions.

--- welcome.sh
+++ welcome.sh
@@ -1,6 +1,5 @@
-echo "Run these two commands"
+echo "Run this command"
 echo
-echo "exec -l $SHELL"
 echo "gcloud init"
 echo 
 echo "This will put you in an authentication handshake. Copy the link it provides you into an incognito browser tabs where you sign in under the Google Account where you have VMs active."
@@ -8,7 +7,7 @@ echo
 echo "Next, set some config variables for the Google Cloud CLI tool. This isn't required, I just like to do it this way."
 echo
 echo "gcloud config set project <project-name>"
-echo "gcloud config set compute/zone <zone-name>"
+echo "gcloud config set compute/zone <zone-name>, i.e. us-central1-b"
 echo
 echo
 echo "gcloud compute ssh <machine-name>"
